86af6bad50582251712fd22a8c68ed5adacde706
Alex Spataru <alex_spataru@outlook.com>
2018-07-27 22:27:16 -0500

    Update Global.h

--- src/Global.h
+++ src/Global.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2018 Alex Spataru <https://github.com/alex-spataru>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
@@ -16,4 +38,4 @@ static const QString HiDPI_FixerHome = QString ("%1/.hidpi-fixer").arg (QDir::ho
 static const QString HiDPI_AutostartBase = "HiDPI-Fixer_";
 static const QString HiDPI_AutostartDir = QString ("%1/.config/autostart").arg (QDir::homePath());
 
-#endif // GLOBAL_H
+#endif
